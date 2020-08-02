bbceee09a46eebc816e0df214377cada93da2a5a
Timothy Grijalva <tgrijalva@gmail.com>
2020-08-02 00:44:32 -0700

    added MIT license

--- goertzel.c
+++ goertzel.c
@@ -2,8 +2,25 @@
 //  goertzel.c
 //  cdsp
 //
-//  Created by Timothy Grijalva on 8/1/20.
-//  Copyright © 2020 Timothy Grijalva. All rights reserved.
+//  Copyright (c) 2020 Timothy Grijalva
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
 //
 
 #include "dsp.h"
@@ -15,11 +32,10 @@
 #define J _Complex_I
 
 // From the Goertzel definition:
-// Y[N] = sum( x[n] * e^(-j*2*pi*k*n/N) ) from n = 0 to N
-// the forulat is broken down into an intermediat IIR filter
-// from n=0 to N-1, and then a final step is calculated
-// where n=N. For details see:
-// https://en.wikipedia.org/wiki/Goertzel_algorithm
+// Y[N] = sum( x[n] * e^(-j*2*pi*k*n/N) ) from n=0 to N.
+// The formula is broken down into an intermediate IIR filter
+// from n=0 to N-1, and an additional step where n=N.
+// For details see: https://en.wikipedia.org/wiki/Goertzel_algorithm
 // The implemataion is normalized to compute Fourier Transform Coefficients W[k],
 // without regard for the sampling frequency, fs.
 double complex goertzel(double *x, int N, int k) {
